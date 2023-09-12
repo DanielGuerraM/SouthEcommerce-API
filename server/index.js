0b0f4d2a422c2cb1789b8741037a42b5237834fa
DanielGuerraM <DanielGuerraCode@gmail.com>
2023-09-11 22:44:17 -0500

    Se agregan las rutas para products

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import roleRoutes from './routes/userAdmin/role.routes.js'
 import permissionRoutes from './routes/userAdmin/permission.routes.js'
 import brandRoutes from './routes/productAdmin/brand.routes.js'
 import categoryRoutes from './routes/productAdmin/category.routes.js'
+import productRoutes from './routes/productAdmin/product.routes.js'
 
 const port = process.env.PORT || '1234'
 const app = express();
@@ -18,6 +19,7 @@ app.use('/api/useradmin', roleRoutes);
 app.use('/api/useradmin', permissionRoutes);
 app.use('/api/productadmin', brandRoutes);
 app.use('/api/productadmin', categoryRoutes);
+app.use('/api/productadmin', productRoutes);
 
 app.listen(port, () => {
   console.log(`Server on port ${port}`);
